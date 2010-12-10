960fcd186b484214e19e0f0280d24fcc55df6f39
Will <anony@mous.me>
2010-12-10 10:47:04 +0000

    Can render without textures

---
+++ g3d_stats.py
@@ -0,0 +1,24 @@
+import struct
+
+class BinaryStream:
+    def __init__(self,filename):
+        self.filename = filename
+        self.f = file(filename,"rb")
+    def read(self,bytes):
+        return self.f.read(bytes)
+    def text64(self):
+        return self.read(64).strip()
+    def unpack(self,fmt):
+        return struct.unpack(fmt,self.read(struct.calcsize(fmt)))[0]
+    def uint8(self):
+        return ord(self.read(1))
+    def uint16(self):
+        return self.unpack("<H")
+    def uint32(self):
+        return self.unpack("<I")
+    def float32(self):
+        return self.unpack("f")
+        
+class Mesh4:
+    def __init__(self,f):
+        
