32c273b3f9144b89076b83ad3905d44cc9d3949f
Will <anony@mous.me>
2011-05-02 00:27:48 +0200

    support for a suffix on output filenames

--- techtree.html.py
+++ techtree.html.py
@@ -5,11 +5,15 @@ from g3d_thumb import *
 
 make_glut()
 
+if len(sys.argv) < 2:
+    sys.exit("usage: [path_to_techtree] {output_suffix}")
 path = sys.argv[1]
 if path[-1] in ('\\','/'): path = path[:-1]
 techtree = os.path.split(path)[1]
 
-page = open("%s.html"%techtree,"w")
+suffix = sys.argv[2]if len(sys.argv) > 2 else ""
+
+page = open("%s%s.html"%(techtree,suffix),"w")
 page.write("<html><head><title>%s</title></head><body>"%techtree)
 
 for faction in os.listdir("%s/factions/"%path):
@@ -22,7 +26,7 @@ for faction in os.listdir("%s/factions/"%path):
             filename_in = model_dir+model
             filename,ext = os.path.splitext(model)
             if ext.lower() != ".g3d": continue
-            filename_out = ("%s_"%faction)+os.path.split(model)[1]+".gif"
+            filename_out = ("%s%s_"%(faction,suffix))+os.path.split(model)[1]+".gif"
             thumb(filename_in,filename_out)
             page.write("<td><img src=\"%s\"/>"%filename_out)
     page.write("</table>")
